e1356fda56eeb35d62b8eb0de2d1a64af269bc09
Luca Belardinelli <belardinelliluca@gmail.com>
2025-04-23 18:20:06 +0200

    Add eseguiEferma function to print a message at regular intervals and stop after a specified time

--- script.js
+++ script.js
@@ -96,5 +96,21 @@ const conta = creaContatoreAutomatico(1000);
 // 🏆 Snack 7
 // Crea una funzione che ferma un timer dopo un certo tempo
 
-//     Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
+//     Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari,
+//      ma si deve fermare dopo il tempo di stop.
 
+
+
+function eseguiEferma(messaggio, avvio, stop) {
+
+    const id = setInterval(() => {
+        console.log(messaggio);
+    }, avvio);
+
+    setTimeout(() => {
+        clearInterval(id);
+    }, stop);
+}
+
+
+eseguiEferma("sto eseguendo...", 1000, 8000)
