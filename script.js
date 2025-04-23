29079df141ed0e3a5f555f21fcd6aa63fef2f850
Luca Belardinelli <belardinelliluca@gmail.com>
2025-04-23 18:21:41 +0200

    Add bonus snacks for countdown, sequential operations, and throttler functions

--- script.js
+++ script.js
@@ -114,3 +114,29 @@ function eseguiEferma(messaggio, avvio, stop) {
 
 
 eseguiEferma("sto eseguendo...", 1000, 8000)
+
+
+
+// 🎯 Snack 8 (Bonus)
+// Crea una funzione che simula un conto alla rovescia
+
+//     Scrivi una funzione contoAllaRovescia che accetta un numero n e stampa il conto alla rovescia da n a 0,
+//      con un intervallo di 1 secondo tra ogni numero. Quando arriva a 0, stampa "Tempo scaduto!" e interrompe il timer.
+
+
+// 🎯 Snack 9 (Bonus)
+// Creare una funzione che esegue una sequenza di operazioni con ritardi
+
+//     Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
+
+//     Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo.
+
+
+
+// 🎯 Snack 10 (Bonus)
+// Creare un throttler per limitare l’esecuzione di una funzione
+
+//     Scrivi una funzione creaThrottler che accetta una funzione e un tempo `limite`.
+
+//     Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
+
